dc64bd4de4a27f7b23d0c3308e80952ea7341cea
Dayron-z <104926781+Dayron-z@users.noreply.github.com>
2024-09-28 14:50:18 -0500

    User and role base is ready.

---
+++ src/main/java/com/pageconnect/booknetwork/role/Role.java
@@ -0,0 +1,43 @@
+package com.pageconnect.booknetwork.role;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.pageconnect.booknetwork.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore // We don't want it to be included in the JSON output to avoid an infinite loop.
+    private List<User> users;
+
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+}
---
+++ src/main/java/com/pageconnect/booknetwork/user/User.java
@@ -0,0 +1,112 @@
+package com.pageconnect.booknetwork.user;
+
+import com.pageconnect.booknetwork.role.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+
+/*In summary, UserDetails is used to encapsulate user information in the context
+of authentication and authorization in spring security.*/
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+/*It automatically manages the creation and modification dates.*/
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails, Principal {
+    @Id
+    @GeneratedValue()
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private boolean accountLocked;
+    private boolean enabled;
+
+    //A user can have multiples roles
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+
+
+
+    //This is unique identifier of our user
+    @Override
+    public String getName() {
+        return email;
+    }
+
+
+    // This method converts the user roles into SimpleGrantedAuthority objects,
+    // which allows Spring Security to interpret them  as authorities.
+    // In this way, we can assign permits to access specific routes or perform actions based on these roles
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+
+    private String fullName(){
+        return firstName + " "  + lastName;
+    }
+}
