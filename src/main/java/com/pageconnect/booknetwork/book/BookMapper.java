b7e050f2fd61f4321eb8cb62925a397160e37eda
Dayron-z <104926781+Dayron-z@users.noreply.github.com>
2024-11-13 10:35:24 -0500

    findAllByOwner is ready

---
+++ src/main/java/com/pageconnect/booknetwork/book/BookMapper.java
@@ -0,0 +1,32 @@
+package com.pageconnect.booknetwork.book;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookMapper {
+    public Book toBook(BookRequest bookRequest) {
+        return Book.builder()
+                .id(bookRequest.id())
+                .title(bookRequest.title())
+                .authorName(bookRequest.authorName())
+                .synopsys(bookRequest.synopsys())
+                .archived(false)
+                .shareable(bookRequest.shareable())
+                .build();
+    }
+
+    public BookResponse toBookResponse(Book book) {
+        return BookResponse.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .authorName(book.getAuthorName())
+                .isbn(book.getIsbn())
+                .synopsys(book.getSynopsys())
+                .rate(book.getRate())
+                .archived(book.isArchived())
+                .shareable(book.isShareable())
+                .owner(book.getOwner().fullName())
+                //.cover()
+                .build();
+    }
+}
---
+++ src/main/java/com/pageconnect/booknetwork/book/BookResponse.java
@@ -0,0 +1,22 @@
+package com.pageconnect.booknetwork.book;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BookResponse {
+    private Integer id;
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsys;
+    private String owner;
+    private byte[] cover;
+    private double rate;
+    private boolean archived;
+    private boolean shareable;
+}
